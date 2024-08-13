0522896911fd4f363190b5da279e872cb533629b
patorodriguez12 <pato_rodriguez12@outlook.com>
2024-08-13 17:23:41 -0300

    undo changes

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,15 +1,14 @@
 const { Router } = require("express");
 const router = Router();
 
-const { getDrivers } = require('../Controllers/getDrivers');
-const getDriverById = require('../Controllers/getDriverById');
-const { postDrivers } = require('../Controllers/postDrivers');
-const { getTeams } = require('../Controllers/getTeams');
+const { getDrivers } = require("../Controllers/getDrivers");
+const getDriverById = require("../Controllers/getDriverById");
+const { postDrivers } = require("../Controllers/postDrivers");
+const { getTeams } = require("../Controllers/getTeams");
 
-
-router.get('/drivers', getDrivers);
-router.get('/drivers/:id', getDriverById);
-router.post('/drivers', postDrivers);
-router.get('/teams', getTeams);
+router.get("/drivers", getDrivers);
+router.get("/drivers/:id", getDriverById);
+router.post("/drivers", postDrivers);
+router.get("/teams", getTeams);
 
 module.exports = router;
