81f0ff9bb12052bc54c6b5a4e6cf18cdd49c5340
patorodriguez12 <pato_rodriguez12@outlook.com>
2024-08-19 10:07:11 -0300

    re-style cards

--- client/src/Components/Views/Home/Card/Card.jsx
+++ client/src/Components/Views/Home/Card/Card.jsx
@@ -1,18 +1,18 @@
-import style from './Card.module.css';
-import { Link } from 'react-router-dom';
-import React from 'react';
+import style from "./Card.module.css";
+import { Link } from "react-router-dom";
+import React from "react";
 
 export default function Card({ driver }) {
-    const { id, image, forename, surname, nationality, dob, teams } = driver;
-    return (
-        <div className={style.card}>
-            <Link to={`/detail/${id}`} className={style.link}>
-                <img src={image} alt="F1 Driver" />
-                <h1>{forename} {surname}</h1>
-                <h2>{teams}</h2>
-                <p>{nationality}</p>
-                <p>{dob}</p>
-            </Link>
-        </div>
-    )
+  const { id, image, forename, surname, nationality } = driver;
+  return (
+    <div className={style.card}>
+      <Link to={`/detail/${id}`} className={style.link}>
+        <img src={image} alt="F1 Driver" />
+        <h1>
+          {forename} {surname}
+        </h1>
+        <p>{nationality}</p>
+      </Link>
+    </div>
+  );
 }
