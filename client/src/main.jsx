51ae2eb4c091df22cfec40640899d2fcc5d88f45
patorodriguez12 <pato_rodriguez12@outlook.com>
2024-08-28 09:35:45 -0300

    store config

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom';
-// import store from './redux/store.js';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import store from "./redux/store.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
-       <App />
+        <App />
       </BrowserRouter>
-  </React.StrictMode>
+    </React.StrictMode>
+  </Provider>
 );
